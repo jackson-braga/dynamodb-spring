d3013f134a6be3491592816c1f596d91e49abf5b
LucasKubo <lucaskbk2@gmail.com>
2022-10-05 13:41:08 -0300

    validacao para request de coluna implementada

--- src/main/java/com/ada/dynamo/dto/request/ColunaRequest.java
+++ src/main/java/com/ada/dynamo/dto/request/ColunaRequest.java
@@ -2,8 +2,13 @@ package com.ada.dynamo.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class ColunaRequest {
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres")
+    @NotNull(message = "Nome vazio")
     private String nome;
     private String cor; //#FFFFFF
     private Integer ordem;
