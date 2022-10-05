8ceeb6bbda687756f53ccdfdb8be008bccfbf07f
derik <degaloni@gmail.com>
2022-10-05 13:18:32 -0300

    Changes by Andressa Oliveira

--- src/main/java/com/ada/dynamo/repository/QuadroRepository.java
+++ src/main/java/com/ada/dynamo/repository/QuadroRepository.java
@@ -2,10 +2,13 @@ package com.ada.dynamo.repository;
 
 import com.ada.dynamo.model.Quadro;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,5 +20,17 @@ public class QuadroRepository {
         quadro.setId(UUID.randomUUID().toString());
         mapper.save(quadro);
         return quadro;
+    }
+
+    public Quadro findById(UUID id) {
+        return mapper.load(Quadro.class, id);
+    }
+
+    public Iterable<Quadro> findAll(){
+        return new ArrayList<>(mapper.scan(Quadro.class, new DynamoDBScanExpression()));
+    }
+
+    public void deleteById(UUID id){
+        mapper.delete(findById(id));
     }
 }
