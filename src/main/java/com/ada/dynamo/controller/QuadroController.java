8c07fb6e7b547eb94f5f8b26dbc9239f81706e03
Jackson Braga Ext (Objective) <jackson.braga.ext@sodexo.com>
2022-10-05 19:15:57 -0300

    Adicionado exemplos de consultas com query e com scan.

--- src/main/java/com/ada/dynamo/controller/QuadroController.java
+++ src/main/java/com/ada/dynamo/controller/QuadroController.java
@@ -4,10 +4,9 @@ import com.ada.dynamo.model.Quadro;
 import com.ada.dynamo.repository.QuadroRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Iterator;
 
 @RestController
 @RequestMapping("/api/quadro")
@@ -18,5 +17,10 @@ public class QuadroController {
     @PostMapping
     public ResponseEntity<Quadro> addQuadro(@RequestBody Quadro quadro){
         return ResponseEntity.ok(repository.save(quadro));
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Quadro>> findAll() {
+        return ResponseEntity.ok(repository.findAll());
     }
 }
--- src/main/java/com/ada/dynamo/repository/QuadroRepository.java
+++ src/main/java/com/ada/dynamo/repository/QuadroRepository.java
@@ -2,10 +2,19 @@ package com.ada.dynamo.repository;
 
 import com.ada.dynamo.model.Quadro;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,5 +26,33 @@ public class QuadroRepository {
         quadro.setId(UUID.randomUUID().toString());
         mapper.save(quadro);
         return quadro;
+    }
+
+    public Iterable<Quadro> findAll() {
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":id", new AttributeValue().withS("a5ca8f74-121b-4f3a-bb85-961118da5bb3"));
+//        eav.put(":tipo", new AttributeValue().withS("QUA"));
+//
+//        DynamoDBQueryExpression<Quadro> queryExpression = new DynamoDBQueryExpression<Quadro>()
+//                .withKeyConditionExpression("id = :id and begins_with(tipo, :tipo)")
+//                .withExpressionAttributeValues(eav);
+//
+//        List<Quadro> quadros = mapper.query(Quadro.class, queryExpression);
+
+//        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+//                .withFilterExpression("begins_with(id, :id)")
+//                .withExpressionAttributeValues(eav);
+
+        Condition contition = new Condition()
+                .withComparisonOperator(ComparisonOperator.BEGINS_WITH)
+                .withAttributeValueList(new AttributeValue().withS("a5ca8f74-121b-4f3a-bb85-961118da5bb3"));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withScanFilter(Map.of("id", contition));
+
+
+        List<Quadro> quadros = mapper.scan(Quadro.class, scanExpression);
+
+        return quadros.stream().collect(Collectors.toList());
     }
 }
