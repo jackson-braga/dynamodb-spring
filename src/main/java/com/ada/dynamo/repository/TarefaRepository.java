2c3da9d690901228bdea43afd7e3aca5ade120c3
LucasKubo <lucaskbk2@gmail.com>
2022-10-06 12:40:47 -0300

    metodo para alterar coluna de cartao-tarefa implementado

--- src/main/java/com/ada/dynamo/repository/TarefaRepository.java
+++ src/main/java/com/ada/dynamo/repository/TarefaRepository.java
@@ -3,10 +3,13 @@ package com.ada.dynamo.repository;
 import com.ada.dynamo.model.Tarefa;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -20,8 +23,13 @@ public class TarefaRepository extends AbstractRepository<Tarefa,String> {
 
     @Override
     protected DynamoDBScanExpression getDynamoDBScanExpression() {
-        //TODO: implementar método
-        return null;
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":tipo", new AttributeValue().withS("TAREFA"));
+        var scanDB = new DynamoDBScanExpression()
+                .withFilterExpression("tipo = :tipo")
+                .withExpressionAttributeValues(eav);
+
+        return scanDB;
     }
 
     @Override
